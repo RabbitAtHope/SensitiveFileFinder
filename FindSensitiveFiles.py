c6f399bd8db72837017ec8004cf554dfb800dec2
Madeleine Rabitoy <rabitoy@hope.edu>
2025-05-21 13:20:54 -0400

    Added file count indicator

--- FindSensitiveFiles.py
+++ FindSensitiveFiles.py
@@ -23,9 +23,11 @@ class bcolors:
     ORANGE = '\033[38;5;208m'
 
 def scan(folder_path):
+    numberScanned = 0
     foundFiles = []
     for root, dirs, files in os.walk(folder_path):
         for file in files:
+            numberScanned += 1
             for extension in sensitiveExtensions:
                 if file.lower().endswith(extension):
                     filePath = os.path.join(root, file)
@@ -33,7 +35,7 @@ def scan(folder_path):
                     filePath = filePath.replace(directory, "").replace("\\","/")
                     print(f" 📄 {bcolors.RED}" + filePath + f"{bcolors.ENDC}")
                     foundFiles.append(filePath)
-    return foundFiles
+    return (foundFiles, numberScanned)
 
 sensitiveExtensions = [
     ".aws",
@@ -56,6 +58,8 @@ directories = [
 ]
 for directory in directories:
     print(f"📁 {bcolors.WARNING}"+directory+f"{bcolors.ENDC}")    
-    files = scan(directory)
+    files, numberScanned = scan(directory)
     if not files:
-        print(f" ✅ {bcolors.OKGREEN}Nothing found.{bcolors.ENDC}")
+        print(f" ✅ {bcolors.OKGREEN}Nothing found.{bcolors.ENDC} ({bcolors.WARNING}"+str(numberScanned)+f" files checked{bcolors.ENDC})")
+    else:
+        print(f" ☣️ {bcolors.OKGREEN}Some files found.{bcolors.ENDC} ({bcolors.WARNING}"+str(numberScanned)+f" files checked{bcolors.ENDC})")
