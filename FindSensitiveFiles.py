ecf8ca6c1e34defbe2837aca71755b157dd7a860
Madeleine Rabitoy <rabitoy@hope.edu>
2025-05-21 13:49:23 -0400

    Code cleanup, more features, added exceptions

--- FindSensitiveFiles.py
+++ FindSensitiveFiles.py
@@ -22,27 +22,35 @@ class bcolors:
     WHITE = '\033[37m'
     ORANGE = '\033[38;5;208m'
 
+def outputFile(root, file, extension):
+    filePath = os.path.join(root, file)
+    filePath = filePath.replace(file, f"{bcolors.FAIL}"+file+f"{bcolors.ENDC}").replace(extension, f"{bcolors.RED}"+extension+f"{bcolors.ENDC}")
+    filePath = filePath.replace(directory, "").replace("\\","/")
+    print(f" 📄 {bcolors.RED}" + filePath + f"{bcolors.ENDC}")
+    return filePath
+
 def scan(folder_path):
     numberScanned = 0
     foundFiles = []
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             numberScanned += 1
+            if file in sensitiveFiles:
+                foundFiles.append(outputFile(root, file, file.split(".")[-1]))
             for extension in sensitiveExtensions:
-                if file.lower().endswith(extension):
-                    filePath = os.path.join(root, file)
-                    filePath = filePath.replace(file, f"{bcolors.FAIL}"+file+f"{bcolors.ENDC}").replace(extension, f"{bcolors.RED}"+extension+f"{bcolors.ENDC}")
-                    filePath = filePath.replace(directory, "").replace("\\","/")
-                    print(f" 📄 {bcolors.RED}" + filePath + f"{bcolors.ENDC}")
-                    foundFiles.append(filePath)
+                if file.lower().endswith(extension) and file not in exceptions:
+                    foundFiles.append(outputFile(root, file, extension))
+
     return (foundFiles, numberScanned)
 
 sensitiveExtensions = [
     ".aws",
     ".bash_history",
+    ".db",
     ".dockercfg",
     ".dockerconfigjson",
     ".env",
+    ".ffs_db",
     ".history",
     ".key",
     ".keys",
@@ -53,6 +61,17 @@ sensitiveExtensions = [
     ".pfx",
     ".pypirc",
 ]
+sensitiveFiles = [
+    "PASSWORD.txt",
+    "PASSWORDS.txt",
+    "password.txt",
+    "passwords.txt",
+    "secret.txt",
+    "secrets.txt",
+]
+exceptions = [
+    "Thumbs.db",
+]
 directories = [
     "C:/Users/Public",
 ]
@@ -60,6 +79,6 @@ for directory in directories:
     print(f"📁 {bcolors.WARNING}"+directory+f"{bcolors.ENDC}")    
     files, numberScanned = scan(directory)
     if not files:
-        print(f"  ✅ {bcolors.OKGREEN}Nothing found.{bcolors.ENDC} ({bcolors.WARNING}"+str(numberScanned)+f" files checked{bcolors.ENDC})")
+        print(f"  ✅ {bcolors.OKGREEN}Nothing found.{bcolors.ENDC} ({bcolors.WARNING}{numberScanned:,} files checked{bcolors.ENDC})")
     else:
-        print(f"  ☣️ {bcolors.OKGREEN}Some files found.{bcolors.ENDC} ({bcolors.WARNING}"+str(numberScanned)+f" files checked{bcolors.ENDC})")
+        print(f"  ☣️ {bcolors.OKGREEN}Some files found.{bcolors.ENDC} ({bcolors.WARNING}{numberScanned:,} files checked{bcolors.ENDC})")
